af0d88d885fdf9539980ed86a71fbcbffc252e1b
mumuruza <53407099+mumuruza@users.noreply.github.com>
2020-12-27 14:16:59 +0100

    finished game

---
+++ Assets/Script/Level/LauncherExtentions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LauncherExtentions
+{
+    private static Dictionary<Vector2Int, Vector2Int> transitions = new Dictionary<Vector2Int, Vector2Int>()
+    {
+        { Vector2Int.right, Vector2Int.up }, 
+        { Vector2Int.up, Vector2Int.left }, 
+        { Vector2Int.left, Vector2Int.down }, 
+        { Vector2Int.down, Vector2Int.right }, 
+    };
+
+    private static Dictionary<Vector2Int, float> angles = new Dictionary<Vector2Int, float>()
+    {
+        { Vector2Int.right, 270},
+        { Vector2Int.up, 0},
+        { Vector2Int.left, 90},
+        { Vector2Int.down, 180},
+        { Vector2Int.up+Vector2Int.left, 0},
+        { Vector2Int.down+Vector2Int.left, 90},
+        { Vector2Int.down+Vector2Int.right, 180},
+        { Vector2Int.up+Vector2Int.right, 270},
+    };
+
+    public static float ToAngle(this Vector2Int vector) 
+    {
+        return angles[vector];
+    }
+
+    public static Vector2Int NextDirection(this Vector2Int vector)
+    {
+        return transitions[vector];
+    }
+
+}
---
+++ Assets/Script/Objects/ColorLauncherPresenter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ColorLauncherPresenter : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    private ColorLauncher _colorLauncher;
+
+    public event Action<ColorLauncher> LauncherClicked;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        LauncherClicked?.Invoke(_colorLauncher);
+    }
+
+    public void SetLauncher(ColorLauncher colorLauncher)
+    {
+        _colorLauncher = colorLauncher;
+        spriteRenderer.color = colorLauncher.Color.ToColor();
+        transform.rotation = Quaternion.Euler(0, 0, colorLauncher.Direction.ToAngle());
+        transform.position = new Vector3(colorLauncher.Position.x, colorLauncher.Position.y);
+    }
+}
